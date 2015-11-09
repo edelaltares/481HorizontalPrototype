c9293dc9d6a5a8feb24d06ccc33124909f8cb501
Edel Altares <edel.altares@gmail.com>
2015-11-08 20:31:24 -0700

    Restarted project using User Controls instead of Pages; missing SearchResults, Sales, and maps.

---
+++ ItemLocator/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ItemLocator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Search search = new Search();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            stack1.Children.Add(search);
+        }
+
+        public void Navigate(UserControl nextPage)
+        {
+            this.Content = nextPage;
+        }
+
+
+        private void ShowMenu(object sender, RoutedEventArgs e)
+        {
+            if (!menu.IsVisible)
+            {
+                menu.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                menu.Visibility = Visibility.Hidden;
+            }
+        }
+        
+        private void HomeButtonClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MapButtonClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SalesButtonClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
